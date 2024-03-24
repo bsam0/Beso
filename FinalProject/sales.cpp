e07f0ee353c5fa399360c0413522a13112a36add
bsam0 <159968265+bsam0@users.noreply.github.com>
2024-03-24 18:21:37 +0300

    Add files via upload

--- FinalProject/sales.cpp
+++ FinalProject/sales.cpp
@@ -1,11 +1,103 @@
 #include "sales.h"
+#include <iostream>
 
-sales::sales()
+Sales::Sales(std::string product, int quantity, double price, double discount)
 {
-    //ctor
+    this->product = product; // this-> is used to access members (variables or functions) of the current object
+    this->quantity = quantity;
+    this->price = price;
+    this->discount = discount;
 }
 
-sales::~sales()
+std::string Sales::getProduct()
 {
-    //dtor
+    return product;
+}
+
+int Sales::getQuantity()
+{
+    return quantity;
+}
+
+double Sales::getPrice()
+{
+    return price;
+}
+
+double Sales::getDiscount()
+{
+    return discount;
+}
+
+double Sales::getTotalPrice()
+{
+    double totalPrice = quantity * price;
+    double discountAmount = totalPrice * (discount / 100);
+    return totalPrice - discountAmount;
+}
+
+double Sales::getProfit()
+{
+    double cost = quantity * price;
+    double sellingPrice = getTotalPrice();
+    return sellingPrice - cost;
+}
+
+double Sales::getProfitPercentage()
+{
+    double cost = quantity * price;
+    double sellingPrice = getTotalPrice();
+    double profit = sellingPrice - cost;
+    return (profit / cost) * 100;
+}
+
+double Sales::calculateProfitPercentage(double price, int quantity)
+{
+    double cost = quantity * price;
+    double sellingPrice = price * quantity;
+    double profit = sellingPrice - cost;
+    return (profit / cost) * 100;
+}
+
+double Sales::calculateLossPercentage(double price, int quantity)
+{
+    double cost = quantity * price;
+    double sellingPrice = price * quantity;
+    double loss = cost - sellingPrice;
+    return (loss / cost) * 100;
+}
+
+void SalesManager::addSale(std::string product, int quantity, double price, double discount)
+{
+    Sales sale(product, quantity, price, discount);
+    salesList.push_back(sale);
+}
+
+double SalesManager::getTotalSales()
+{
+    double totalSales = 0;
+    for (Sales sale : salesList)
+    {
+        totalSales += sale.getTotalPrice();
+    }
+    return totalSales;
+}
+
+double SalesManager::getTotalProfit()
+{
+    double totalProfit = 0;
+    for (Sales sale : salesList)
+    {
+        totalProfit += sale.getProfit();
+    }
+    return totalProfit;
+}
+
+void SalesManager::displaySales()
+{
+    std::cout << "Product\t\tQuantity\tPrice\tDiscount\tTotal Price\tProfit\tProfit %" << std::endl;
+    for (Sales sale : salesList)
+    {
+        std::cout << sale.getProduct() << "\t" << sale.getQuantity() << "\t\t" << sale.getPrice() << "\t" << sale.getDiscount() << "\t\t" << sale.getTotalPrice() << "\t\t" << sale.getProfit() << "\t" << sale.getProfitPercentage() << "%" << std::endl;
+    }
 }
--- FinalProject/sales.h
+++ FinalProject/sales.h
@@ -1,16 +1,40 @@
 #ifndef SALES_H
 #define SALES_H
 
+#include <string>
+#include <vector>
 
-class sales
+class Sales
 {
-    public:
-        sales();
-        virtual ~sales();
+private:
+    std::string product;
+    int quantity;
+    double price;
+    double discount;
 
-    protected:
-
-    private:
+public:
+    Sales(std::string product, int quantity, double price, double discount);
+    std::string getProduct();
+    int getQuantity();
+    double getPrice();
+    double getDiscount();
+    double getTotalPrice();
+    double getProfit();
+    double getProfitPercentage();
+    static double calculateProfitPercentage(double price, int quantity);
+    static double calculateLossPercentage(double price, int quantity);
 };
 
-#endif // SALES_H
+class SalesManager
+{
+private:
+    std::vector<Sales> salesList;
+
+public:
+    void addSale(std::string product, int quantity, double price, double discount);
+    double getTotalSales();
+    double getTotalProfit();
+    void displaySales();
+};
+
+#endif
